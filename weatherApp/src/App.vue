2f849f5c862e1030fefaefe7f04017106875663e
KKHoon210417 <zozo1591@naver.com>
2022-06-12 21:57:42 +0900

    feat: 프로젝트 생성

---
+++ weatherApp/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <HeaderWeatherApp v-bind:propsdata="currentData"></HeaderWeatherApp>
+    <BodyWeatherApp></BodyWeatherApp>
+    <FooterWeatherApp></FooterWeatherApp>
+    <button v-on:click="getWeatherData">ddd</button>
+  </div>
+</template>
+
+<script>
+import HeaderWeatherApp from "./components/HeaderWeatherApp.vue"
+import BodyWeatherApp from "./components/BodyWeatherApp.vue"
+import FooterWeatherApp from "./components/FooterWeatherApp.vue"
+
+export default {
+  data() {
+    return {
+      currentData: {
+        currentTemp: 10,
+        minTemp: 5,
+        maxTemp: 15,
+        location: "서울",
+        weatherDesc: "구름이 많이 낍니다."
+      }
+    }
+  },
+  components: {
+    'HeaderWeatherApp': HeaderWeatherApp,
+    'BodyWeatherApp': BodyWeatherApp,
+    'FooterWeatherApp': FooterWeatherApp
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #62626a;
+  color: white;
+}
+
+</style>
---
+++ weatherApp/src/components/HeaderWeatherApp.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="headerContainer">
+        <div class="location">{{ propsdata.location }}</div>
+        <div class="title">{{ propsdata.currentTemp }}</div>
+        <div class="description">{{ propsdata.weatherDesc }}</div>
+        <div class="mini-description">최고: {{ propsdata.maxTemp }} 최저: {{ propsdata.minTemp }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['propsdata']
+
+}
+</script>
+
+<style scoped>
+.headerContainer .title {
+    font-size: 100px;
+}
+
+.headerContainer .location {
+    font-size: 50px;
+    margin-bottom: 5px;
+    margin-top: 2.5rem;
+}
+
+.headerContainer .description {
+    font-size: 30px;
+}
+
+.headerContainer .mini-description {
+    font-size: 20px;
+}
+
+
+.headerContainer {
+    margin-top: 100px;
+}
+
+</style>
